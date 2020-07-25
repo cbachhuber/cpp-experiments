67a71ae097fcac3ac2afffe29803fdd33339c1c9
Christoph Bachhuber <cbachhuber89@gmail.com>
2020-07-25 09:01:48 +0200

    Further modernize-avoid-bind investigation

--- std_bind_vs_lambda.cpp
+++ std_bind_vs_lambda.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <functional>
 
 int subtract(int a, int b) { return a - b; }
@@ -8,11 +9,17 @@ int main() {
 
     assert(inverted_subtract_bind(1, 2) == inverted_subtract_lambda(1, 2));
 
-    // Issue: `clang-tidy std_bind_vs_lambda.cpp` suggests
+    // `clang-tidy std_bind_vs_lambda.cpp --checks='-*,modernize-avoid-bind'` suggests
     //    ```cpp
     //    const auto inverted_subtract_bind = [] {
     //        return subtract(std::placeholders::_2, std::placeholders::_1);
     //    };
     //    ```
-    // which is not correct. It should suggest the implementation of `inverted_subtract_lambda` above
+    // which is not correct. Enabling option 'PermissiveParameterList' in .clang-tidy does not help:
+    //    ```cpp
+    //    const auto inverted_subtract_bind = [](auto && ...) {
+    //        return subtract(std::placeholders::_2, std::placeholders::_1);
+    //    };
+    //    ```
+    // clang-tidy should suggest something similar to the implementation of `inverted_subtract_lambda` above.
 }
