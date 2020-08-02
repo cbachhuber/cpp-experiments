54106691ee99a0a2b9b682962b8c89655110bfd4
Christoph Bachhuber <cbachhuber89@gmail.com>
2020-08-02 17:11:00 +0200

    Perfect forwarding

--- std_bind_vs_lambda.cpp
+++ std_bind_vs_lambda.cpp
@@ -1,11 +1,14 @@
 #include <cassert>
 #include <functional>
+#include <utility>
 
 int subtract(int a, int b) { return a - b; }
 
 int main() {
     const auto inverted_subtract_bind = std::bind(subtract, std::placeholders::_2, std::placeholders::_1);
-    const auto inverted_subtract_lambda = [](auto _1, auto _2) { return subtract(_2, _1); };
+    const auto inverted_subtract_lambda = [](auto&& _1, auto&& _2) {
+        return subtract(std::forward<decltype(_2)>(_2), std::forward<decltype(_1)>(_1));
+    };
 
     assert(inverted_subtract_bind(1, 2) == inverted_subtract_lambda(1, 2));
 
