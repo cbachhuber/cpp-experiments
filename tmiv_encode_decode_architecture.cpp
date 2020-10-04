ee9c802fc34ef64744e05f1455e88247dd37e9d5
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-10-04 16:12:58 +0200

    Extract functions in encodeTo and decodeFrom

--- tmiv_encode_decode_architecture.cpp
+++ tmiv_encode_decode_architecture.cpp
@@ -1,21 +1,68 @@
 
 #include <cassert>
 #include <cstdint>
+#include <functional>
 #include <iostream>
 #include <optional>
+#include <type_traits>
+
+constexpr std::size_t bits_int = 27;
+constexpr std::size_t bits_optional_unsigned = 5;
+
+template <typename T>
+auto putField(std::ostream& stream, const std::string& fieldName, T&& fieldValue) {
+    stream << fieldName << "=";
+    if constexpr (std::is_same_v<std::uint8_t, std::decay_t<T>>) {
+        stream << static_cast<unsigned>(fieldValue) << "\n";
+    } else if (std::is_same_v<bool, std::decay_t<T>>) {
+        stream << std::boolalpha << fieldValue << "\n";
+    } else {
+        stream << fieldValue << "\n";
+    }
+}
 
 class Bitstream {
   public:
-    void putFlag(bool /*flag*/) {}
+    void putFlag(bool /*flag*/) { std::cout << "Called putFlag\n"; }
     template <typename T>
-    void writeBits(std::size_t /*number_of_bits*/) {}
+    void writeBits(T /*data*/, std::size_t number_of_bits) {
+        std::cout << "Called writeBits with " << number_of_bits << "bits\n";
+    }
 
-    bool getFlag() { return true; }
+    bool getFlag() {
+        std::cout << "Called getFlag\n";
+        return true;
+    }
     template <typename T>
-    T readBits(std::size_t /*number_of_bits*/) {
+    T readBits(std::size_t number_of_bits) {
+        std::cout << "Called readBits with " << number_of_bits << "bits\n";
         return {};
     }
 };
+
+// Option: this function could take the string fieldName and not use it!
+template <typename FunctionType>
+void writeToStream(Bitstream& stream, FunctionType&& function, std::size_t number_of_bits = 0) {
+    if constexpr (std::is_same_v<bool, decltype(function())>) {
+        stream.putFlag(function());
+        // The below two could actually be summarized in an else statement
+    } else if constexpr (std::is_same_v<std::int32_t, decltype(function())>) {
+        stream.writeBits<decltype(function())>(function(), number_of_bits);
+    } else if constexpr (std::is_same_v<std::uint8_t, decltype(function())>) {
+        stream.writeBits<decltype(function())>(function(), number_of_bits);
+    }
+}
+
+// TODO(CB) extend to indexed (multi argument) setters
+// TODO(CB) this function signature is too constraining. You want to generalize to returning functions!
+template <typename ArgumentType>
+void readFromStream(Bitstream& stream, std::function<void(ArgumentType)> setter, std::size_t number_of_bits = 0) {
+    if constexpr (std::is_same_v<bool, ArgumentType>) {
+        setter(stream.getFlag());
+    } else {
+        setter(stream.readBits<ArgumentType>(number_of_bits));
+    }
+}
 
 class Message {
   public:
@@ -31,33 +78,53 @@ class Message {
     void my_optional(std::uint8_t val) { m_optional_uint = val; }
 
     friend std::ostream& operator<<(std::ostream& stream, const Message& msg) {
-        stream << "my_int=" << msg.my_int() << "\n";
-        stream << "my_flag=" << msg.my_flag() << "\n";
+        putField(stream, "my_int", msg.my_int());
+        putField(stream, "my_flag", msg.my_flag());
         if (msg.my_flag()) {
-            stream << "my_optional=" << static_cast<unsigned>(msg.my_optional()) << "\n";
+            putField(stream, "my_optional", msg.my_optional());
         }
         return stream;
     }
 
     static Message decodeFrom(Bitstream& stream) {
         Message result{};
-        result.my_int(stream.readBits<std::int32_t>(27));
-        result.my_flag(stream.getFlag());
+        // TODO(CB) can we get away without those lambdas? Is the current lambda requirement an architecture smell?
+        const std::function<void(std::int32_t)> set_my_int = [&result](std::int32_t value) { result.my_int(value); };
+        const std::function<void(std::uint8_t)> set_my_optional = [&result](std::uint8_t value) {
+            result.my_optional(value);
+        };
+        const std::function<void(bool)> set_my_flag = [&result](bool value) { result.my_flag(value); };
+
+        readFromStream(stream, set_my_int, bits_int);
+        readFromStream(stream, set_my_flag);
         if (result.my_flag()) {
-            result.my_optional(stream.readBits<std::uint8_t>(5));
+            readFromStream(stream, set_my_optional, bits_optional_unsigned);
         }
         return result;
     }
 
     void encodeTo(Bitstream& stream) const {
-        stream.writeBits<std::int32_t>(27);
-        stream.putFlag(my_flag());
-        if(my_flag()){
-            stream.writeBits<std::uint8_t>(5);
+        const auto my_int_lambda = [this]() { return this->my_int(); };
+        const auto my_flag_lambda = [this]() { return this->my_flag(); };
+        const auto my_optional_lambda = [this]() { return this->my_optional(); };
+
+        writeToStream(stream, my_int_lambda, bits_int);
+        writeToStream(stream, my_flag_lambda);
+        if (my_flag()) {
+            writeToStream(stream, my_optional_lambda, bits_optional_unsigned);
         }
     }
 
-    // TODO(cb) callSyntaxOnFunction() or so?
+    // Not void for decodeFrom! But that can be deduced
+    //    void callSyntaxOnFunction(stream (bitstream or ostream) /*of function*/) {
+    //        // Type stored in return type of getter
+    //        function(my_int, 27); // give function handle, not result!
+    //        function(my_flag);
+    //        if (my_flag()) {
+    //            function(my_optional, 5);
+    //        }
+    //    }
+
   private:
     std::int32_t m_my_int{};
     bool m_flag_for_optional{};
